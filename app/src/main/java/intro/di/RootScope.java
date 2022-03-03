dacaf62701b976366cfa4959dcc1e3354fe6835e
Tony Tang <tonytang@uber.com>
2022-03-03 11:40:15 -0800

    Remove package.

--- app/src/main/java/intro/di/RootScope.java
+++ app/src/main/java/intro/di/RootScope.java
@@ -3,8 +3,6 @@ package intro.di;
 import intro.di.network.AuthService;
 import intro.di.network.AuthServiceImpl;
 import intro.di.network.NetworkClient;
-import intro.di.network.RideRequestService;
-import intro.di.network.RideRequestServiceImpl;
 import motif.Expose;
 import motif.Scope;
 
