1fb58347edf88d4dac4450da32c7da3fdccff32f
TonyTang <tonythompsoncmu@gmail.com>
2022-04-27 12:42:06 -0700

    [step 5_3]:  Update the unit test to build RideScope through AuthenticationScope

--- app/src/test/java/com/example/intromotif/ScopeTest.java
+++ app/src/test/java/com/example/intromotif/ScopeTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import intro.di.Profile;
 import intro.di.network.AuthenticationScope;
 import intro.di.network.AuthenticationScopeImpl;
+import intro.di.network.RideRequestService;
 import intro.di.network.RideScope;
 import intro.di.network.RideScopeImpl;
 
@@ -31,5 +32,13 @@ public class ScopeTest {
     public void authenticationScope() {
         AuthenticationScope authenticationScope = new AuthenticationScopeImpl();
         Truth.assertThat(authenticationScope).isNotNull();
+
+        Profile profile = authenticationScope.profile();
+        RideScope rideScope = authenticationScope.rideScope(profile);
+
+        RideRequestService rideRequestService = rideScope.rideRequestService();
+
+        //final request the ride.
+        rideRequestService.requestRide();
     }
 }
