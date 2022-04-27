381d2e94876beb7c549d723584aaceaebb9c3920
TonyTang <tonythompsoncmu@gmail.com>
2022-04-27 12:27:27 -0700

    [step 4]: Add a second NetworkClient constructor to test out motif error.

--- app/src/main/java/intro/di/network/NetworkClient.java
+++ app/src/main/java/intro/di/network/NetworkClient.java
@@ -1,8 +1,17 @@
 package intro.di.network;
 
+import javax.inject.Inject;
+
 public class NetworkClient {
 
+  //by default, if there is only one constructor, motif will use the only constructor.
+  //Of all di concept, we must identify the one and only one path.
+
+  //if there are two constructors, you have to inject the desired one.
   public NetworkClient() {
+  }
+
+  public NetworkClient(String x) {
   }
 
   public String newCall(String url) {
