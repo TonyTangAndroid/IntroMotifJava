62948beee66e9faa6d8308f38068483b1bc82689
TonyTang <tonythompsoncmu@gmail.com>
2022-04-27 12:40:21 -0700

    [step 5_2]: Add  child scope concept in AuthenticationScope to connect AuthenticationScope and RideScope

--- app/src/main/java/intro/di/network/AuthenticationScope.java
+++ app/src/main/java/intro/di/network/AuthenticationScope.java
@@ -9,6 +9,12 @@ public interface AuthenticationScope {
 
     Profile profile();
 
+    //child scope.
+    //to create a ride scope, we need to satisfy the Profile requirement.
+    //hence, we pass [Profile] as a parameter.
+    //This parameter is called dynamic dependency.
+    RideScope rideScope(Profile profile);
+
 
     @motif.Objects
     abstract class Objects {
