c0b4f47c85dcfb7d478ad540a9830ab475de3bc6
TonyTang <tonytang@uber.com>
2023-02-23 13:37:52 -0800

    15:Add Inject on a different constructor will trigger a new dependency requirement.

--- app/src/main/java/intro/di/network/NetworkClient.java
+++ app/src/main/java/intro/di/network/NetworkClient.java
@@ -4,14 +4,16 @@ import javax.inject.Inject;
 
 public class NetworkClient {
 
-  @Inject
+
   public NetworkClient() {
 
   }
 
+  @Inject
   public NetworkClient(String serverUrl) {
 
   }
+
   public String newCall(String url) {
     return String.format("%s is ok", url);
   }
