60539df35453ac94258cb3bfc7c9c963dd412f27
TonyTang <tonytang@uber.com>
2023-02-23 13:11:33 -0800

    3: Add comments for the empty scope.

--- app/src/main/java/intro/di/RootScope.java
+++ app/src/main/java/intro/di/RootScope.java
@@ -1,5 +1,10 @@
 package intro.di;
 
+/**
+ * Having a empty scope is a great start.
+ * It means your code compiles
+ * It means motif setup works.
+ */
 @motif.Scope
 public interface RootScope {
 
