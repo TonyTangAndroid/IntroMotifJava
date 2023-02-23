e2cc515e6203b35c0f76b0f171383e4a839de8c8
TonyTang <tonytang@uber.com>
2023-02-23 13:33:08 -0800

    13:Add two constructors to trigger a motif error.

--- app/src/main/java/intro/di/network/NetworkClient.java
+++ app/src/main/java/intro/di/network/NetworkClient.java
@@ -4,7 +4,13 @@ import javax.inject.Inject;
 
 public class NetworkClient {
 
+  public NetworkClient() {
 
+  }
+
+  public NetworkClient(String serverUrl) {
+
+  }
   public String newCall(String url) {
     return String.format("%s is ok", url);
   }
