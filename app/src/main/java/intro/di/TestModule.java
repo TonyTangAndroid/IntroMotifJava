20c9e15e543ff9219e7ec495dce8902be19ac8ac
TonyTang <tonytang@uber.com>
2023-02-23 13:16:41 -0800

    5:Add tips for compile safety.

--- app/src/main/java/intro/di/TestModule.java
+++ app/src/main/java/intro/di/TestModule.java
@@ -6,6 +6,11 @@ import intro.di.network.AuthService;
 @dagger.Module
 public abstract class TestModule {
 
+  /**
+   * Both Motif and Dagger are compile safety.
+   * But it does not check run time exceptions.
+   * Hence, you could use `throw new RuntimeException();` to divide and conque your task.
+   */
   @Provides
   public static AuthService authService(){
     throw new RuntimeException();
